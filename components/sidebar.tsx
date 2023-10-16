35e03a4f0a406838d9944c66074e0018761f07d3
Leonardo Santa Cruz <leonardo.santacruz@ibisdev.tech>
2023-10-16 16:54:47 +0200

    First commit

---
+++ components/sidebar.tsx
@@ -0,0 +1,86 @@
+import {
+  changeColor,
+  emissionAnimation,
+  addTexture,
+  setTexture,
+  setBumpMap,
+  getMaterials,
+} from "@/utils/sketchfab";
+
+interface Props {
+  changeScenario: (val: string) => void;
+}
+
+const Sidebar = (props: Props) => {
+  const emissionHoveranimation = (part: string) => {
+    emissionAnimation(part);
+  };
+
+  const colors = [
+    { name: "white", r: 255, g: 247, b: 232 },
+    { name: "black", r: 0, g: 0, b: 0 },
+    { name: "green", r: 25, g: 61, b: 50 },
+    { name: "red", r: 83, g: 32, b: 44 },
+  ];
+  const defaultTextureUid =
+    getMaterials().filter((r) => {
+      return r.name == "Leather_001";
+    })[0]?.channels.NormalMap.texture.uid || "";
+
+  const updateColor = async (name: string, r: number, g: number, b: number) => {
+    //let textureUID = "";
+    let bumpMapUID = defaultTextureUid;
+
+    if (name == "green") {
+      bumpMapUID = await addTexture("https://i.imgur.com/Tn2UYDu.jpg");
+      //textureUID = await addTexture("https://i.imgur.com/wHFrCPy.jpg");
+    }
+    //setTexture("Leather_001", textureUID);
+    console.log(bumpMapUID);
+    setBumpMap("Leather_001", bumpMapUID);
+    changeColor("Leather_001", { r, g, b });
+    changeColor("Leather_002", { r, g, b });
+  };
+
+  return (
+    <>
+      <aside className="flex flex-col text-left p-10 ">
+        <h1 className="text-4xl mt-10">Sardine</h1>
+
+        <h2 className="text-2xl mt-3">3500 €</h2>
+
+        <p className="my-10 text-xl">
+          Bag made from supple lambskin leather using Intrecciato Craftsmanship
+          featuring sculptural Read more
+        </p>
+
+        <p className="font-bold text-xl">
+          Colors:
+          <div className="flex flex-wrap gap-5 my-4">
+            {colors.map(({ name, r, g, b }) => {
+              return (
+                <div
+                  onClick={() => {
+                    updateColor(name, r, g, b);
+                  }}
+                  className="w-[40px] h-[40px] rounded-full cursor-pointer hover:opacity-70"
+                  style={{ backgroundColor: `rgb(${r},${g},${b})` }}
+                ></div>
+              );
+            })}
+          </div>
+        </p>
+        <div className="w-[300px] flex flex-col mt-10">
+          <button className="p-3 border border-2 border-solid border-black">
+            Add to bag
+          </button>
+          <button className="p-3 mt-5 bg-black text-white border border-2 border-solid border-black">
+            Check in-store availability
+          </button>
+        </div>
+      </aside>
+    </>
+  );
+};
+
+export default Sidebar;
